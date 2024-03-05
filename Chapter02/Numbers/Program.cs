434b4910d3028884fee155ec78b888bcdeed1fff
Stephen Hodgson <stephen@srhodgson.co.uk>
2024-03-05 22:23:04 +0000

    Add project for working with numbers

---
+++ Chapter02/Numbers/Program.cs
@@ -0,0 +1,11 @@
+﻿// An unused integer is a positive whole number or 0.
+uint naturalNumber = 23;
+// An integer is a negative or positive whole number or 0.
+int integerNumber = -23;
+// A float is a single-precision floating-point number.
+// The F or f suffix makes the value a float literal.
+// The suffix is required to compile.
+float realNumber = 2.3f;
+// A double is a double-precision floating-point number.
+// double is the default for a number value with a decimal point.
+double anotherRealNumber = 2.3; // A double literal value.
