bd5216e2dc2023c81ee1abca6b423c74d4cd8928
Stephen Hodgson <stephen@srhodgson.co.uk>
2024-03-05 22:32:20 +0000

    Working with whole numbers

--- Chapter02/Numbers/Program.cs
+++ Chapter02/Numbers/Program.cs
@@ -9,3 +9,18 @@ float realNumber = 2.3f;
 // A double is a double-precision floating-point number.
 // double is the default for a number value with a decimal point.
 double anotherRealNumber = 2.3; // A double literal value.
+
+int decimalNotation = 2_000_000;
+int binaryNotation = 0b_0001_1110_1000_0100_1000_0000;
+int hexadecimalNotation = 0x_001E_8480;
+// Check the three variables have the same value.
+Console.WriteLine($"{decimalNotation == binaryNotation}");
+Console.WriteLine($"{decimalNotation == hexadecimalNotation}");
+// Output the variable values in decimal.
+Console.WriteLine($"{decimalNotation:N0}");
+Console.WriteLine($"{binaryNotation:N0}");
+Console.WriteLine($"{hexadecimalNotation:N0}");
+// Output the variable values in hexadecimal
+Console.WriteLine($"{decimalNotation:X}");
+Console.WriteLine($"{binaryNotation:X}");
+Console.WriteLine($"{hexadecimalNotation:X}");
